0f423227229256278ecc38d74f3b740aa2580a53
Carlos Sevilla <carlossevillasalcedo@gmail.com>
2022-03-01 19:48:26 +0100

    Added interactive canvas and updated storage

--- Inputs.py
+++ Inputs.py
@@ -64,7 +64,11 @@ class manual_mlbl:
             save_path = asksaveasfile(defaultextension = '.txt', filetypes = [('Text file', '.txt'), ('CSS file', '.css'), ('All Files', '*.*')])
         if save_path is not None: # asksaveasfile return `None` if dialog closed with "cancel".
             filedata = pd.DataFrame(self.binary_data, columns = self.class_list).to_string()
+            save_path.seek(0) # Move to the first row to overwrite it
             save_path.write(filedata)
+            save_path.flush() # Save without closing
+            # typically the above line would do. however this is used to ensure that the file is written
+            os.fsync(save_path.fileno())
             saved = True
             
     def forward_back(self, image_number):
@@ -159,3 +163,139 @@ class upload_mlbl:
         master.filename = askopenfilename(initialdir = os.getcwd(), title = 'Select a file', defaultextension = '.txt', filetypes = [('Text file', '.txt'), ('CSS file', '.css'), ('All Files', '*.*')])
         if master.filename is not None:
             messagebox.showinfo("Success", "Feedback correctly uploaded.")
+            
+class canvas_sa:
+
+    global save_path
+    global saved
+    global selected
+    save_path = ''
+    saved = True
+        
+    def draw_dot(self, event):    
+        
+        if self.draw.get() == 'drag':
+            selected = self.canvas.find_overlapping(event.x-10, event.y-10, event.x+10, event.y+10)
+            if selected:
+                self.canvas.selected = selected[-1]  # select the top-most item
+                self.canvas.startxy = (event.x, event.y)
+            else:
+                self.canvas.selected = None
+
+        if self.draw.get()  == 'draw':
+            # Draw an oval in the given co-ordinates
+            self.canvas.create_oval(event.x, event.y, event.x, event.y, fill="black", width=5)
+            global saved
+            saved = False
+            self.out_data['x'].append(event.x)
+            self.out_data['y'].append(event.y)
+
+    def on_drag(self, event):
+        if self.draw.get()  == 'drag' and self.canvas.selected:
+            # calculate distance moved from last position
+            dx, dy = event.x-self.canvas.startxy[0], event.y-self.canvas.startxy[1]
+            # move the selected item
+            self.canvas.move(self.canvas.selected, dx, dy)
+            # update last position
+            self.canvas.startxy = (event.x, event.y)
+            
+    def checkered(self, line_distance):        
+        # vertical lines at an interval of "line_distance" pixel
+        for x in range(line_distance, self.width, line_distance):
+            self.canvas.create_line(x, 0, x, self.height, fill="#476042")
+        # horizontal lines at an interval of "line_distance" pixel
+        for y in range(line_distance, self.height, line_distance):
+            self.canvas.create_line(0, y, self.width, y, fill="#476042")   
+            
+    def check_quit(self, master):
+        global saved
+        if not saved:
+            response = messagebox.askokcancel("Are you sure you want to leave?", "Do you want to leave the program without saving?")
+            if response:
+                master.destroy()
+        else:
+            master.destroy()
+            
+    def save_file_as(self):
+        global save_path
+        save_path = asksaveasfile(mode='w')
+        save_file()
+        
+    def save_file(self):
+        global save_path
+        global saved
+        if save_path == '':
+            save_path = asksaveasfile(defaultextension = '.txt', filetypes = [('Text file', '.txt'), ('CSS file', '.css'), ('All Files', '*.*')])
+        if save_path is not None: # asksaveasfile return `None` if dialog closed with "cancel".
+            filedata = pd.DataFrame(self.out_data, columns = ['x', 'y']).to_string()
+            save_path.seek(0) # Move to the first row to overwrite it
+            save_path.write(filedata)
+            save_path.flush() # Save without closing
+            # typically the above line would do. however this is used to ensure that the file is written
+            os.fsync(save_path.fileno())
+            saved = True
+    
+    def upload_sa(self):
+        filename = askopenfilename(initialdir = os.getcwd(), title = 'Select a file', defaultextension = '.txt', filetypes = [('Text file', '.txt'), ('CSS file', '.css'), ('All Files', '*.*')])
+        if filename is not None:
+            data = open(filename,'r')
+            read = False
+            for point in data:
+                if read:
+                    i, x, y = point.split()
+                    # Draw an oval in the given co-ordinates
+                    self.canvas.create_oval(x, y, x, y,fill="grey", width=5)
+                    self.out_data['x'].append(x)
+                    self.out_data['y'].append(y)
+                else:
+                    read = True
+    def reset(self):
+        msg = messagebox.askyesnocancel('Info', 'Are you sure you want to reset the canvas?')
+        if msg:
+            self.canvas.delete(ALL)
+            self.checkered(10)
+            self.out_data = {'x': [], 'y': []} #coordinates
+            
+    #windows zoom
+    def zoomer(self,event):
+        if (event.delta > 0):
+            self.canvas.scale("all", event.x, event.y, 1.1, 1.1)
+        elif (event.delta < 0):
+            self.canvas.scale("all", event.x, event.y, 0.9, 0.9)
+        self.canvas.configure(scrollregion = self.canvas.bbox("all"))
+
+    #linux zoom
+    def zoomerP(self,event):
+        self.canvas.scale("all", event.x, event.y, 1.1, 1.1)
+        self.canvas.configure(scrollregion = self.canvas.bbox("all"))
+    def zoomerM(self,event):
+        self.canvas.scale("all", event.x, event.y, 0.9, 0.9)
+        self.canvas.configure(scrollregion = self.canvas.bbox("all"))
+        
+    def __init__(self, master):
+        self.out_data = {'x': [], 'y': []} #coordinates
+        
+        # Create a canvas widget
+        self.width, self.height = 600, 600
+        self.canvas=Canvas(master, width=self.width, height=self.height, background="white")
+        self.canvas.grid(row=0, column=0, columnspan=4)
+        self.checkered(10)
+        self.canvas.bind('<Button-1>', self.draw_dot)
+        self.canvas.bind("<B1-Motion>", self.on_drag)
+        #linux scroll
+        self.canvas.bind("<Button-4>", self.zoomerP)
+        self.canvas.bind("<Button-5>", self.zoomerM)
+        #windows scroll
+        self.canvas.bind("<MouseWheel>",self.zoomer)
+        
+        self.draw = StringVar()
+        self.draw.set('draw')
+        # Buttons and text under the canvas
+        # message = Label( master, text = "Click to draw", fg = 'white' , bg = master['bg']).grid(row=19, column=1, columnspan = 2)
+        # variable = IntVar() 
+        self.button_draw = Radiobutton(master, text = 'Draw', fg = 'white', bg = master['bg'], height = 3, width = 20, var = self.draw, selectcolor = 'black', value = 'draw').grid(column = 0,row = 18, columnspan = 2)
+        self.button_drag = Radiobutton(master, text = 'Move', fg = 'white', bg = master['bg'], height = 3, width = 20, var = self.draw, value = 'drag', selectcolor = 'black').grid(column = 2,row = 18, columnspan = 2)
+        self.button_save = Button(master, text = 'Save', fg = 'white', bg = master['bg'], height = 3, width = 20, command = self.save_file).grid(column = 1,row = 19)
+        self.button_quit = Button(master, text = 'Exit', fg = 'white', bg = master['bg'], height = 3, width = 20, command = lambda: self.check_quit(master)).grid(column = 3,row = 19)
+        self.button_upload = Button(master, text = 'Upload coordinates', fg = 'white', bg = master['bg'], height = 3, width = 20, command = self.upload_sa).grid(column = 0, row = 19)
+        self.button_reset = Button(master, text = 'Reset', fg = 'white', bg = master['bg'], height = 3, width = 20, command = self.reset).grid(column = 2, row = 19)
