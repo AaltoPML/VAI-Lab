1bbf1d131ecb7ef89def5060999a7a3ce06ea554
Carlos Sevilla Salcedo <carlossevillasalcedo@gmail.com>
2022-03-25 16:55:03 +0200

    Update to have more than one object
    This version allows having more than one object simultaneously in the canvas.
    Besides, this version starts to adhere to the PEP-8 standard.

--- main.py
+++ main.py
@@ -10,18 +10,18 @@ from tkinter import font  as tkfont # python 3
 import os
 from PIL import Image, ImageTk
 from tkinter import messagebox
-from tkinter.filedialog import asksaveasfile, askopenfile, askopenfilename
-import numpy as np
-import pandas as pd
-from Inputs import PageManual, PageCanvas
+from tkinter.filedialog import askopenfilename
+
+from Inputs_general import PageManual, PageCanvas
 
 class UFA(tk.Tk):
+    """  """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.title_font = tkfont.Font(family='Helvetica', size=14, weight="bold")#, slant="italic")
+        self.title_font = tkfont.Font(family='Helvetica', 
+                                      size=14, weight="bold")#, slant="italic")
         self.pages_font = tkfont.nametofont("TkDefaultFont")
-
 
         # the container is where we'll stack a bunch of frames
         # on top of each other, then the one we want visible
@@ -30,7 +30,7 @@ class UFA(tk.Tk):
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
-
+        
         self.frames = {}
         for F in (StartPage, PageManual, PageCanvas):
             page_name = F.__name__
@@ -49,7 +49,6 @@ class UFA(tk.Tk):
         frame = self.frames[page_name]
         frame.tkraise()
 
-
 class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
@@ -58,29 +57,44 @@ class StartPage(tk.Frame):
         super().__init__(parent, bg = parent['bg'])
         self.controller = controller
         self.controller.title('User feedback adaptation')
-        # self.controller.iconbitmap(os.path.join(os.getcwd(),'Icons','UFAIcon.ico'))
+        self.controller.iconbitmap(
+            os.path.join(os.getcwd(),'Icons','UFAIcon.ico'))
         
-        self.my_img1 = ImageTk.PhotoImage(Image.open(os.path.join(os.getcwd(), 'Icons', 'UFAIcon_name.png')).resize((600, 400)))
+        self.my_img1 = ImageTk.PhotoImage(Image.open(os.path.join(
+                os.getcwd(), 'Icons', 'UFAIcon_name.png')).resize((600, 400)))
         self.my_label = tk.Label(self, image = self.my_img1, bg = parent['bg'])
         self.my_label.grid(column = 0, row = 0, rowspan = 10, columnspan = 4)
         
-        my_label = tk.Label(self, text = 'Choose your prefered method to give feedback to the model.', 
-                            pady= 10, font = controller.title_font, bg = parent['bg'], fg = 'white')
+        my_label = tk.Label(
+            self, 
+            text = 'Choose your prefered method '
+            +'to give feedback to the model.', 
+            pady= 10, font = controller.title_font, 
+            bg = parent['bg'], fg = 'white')
         my_label.grid(column = 0, row = 11,columnspan = 4)
 
-        button_speak = tk.Button(self, text = 'Speak', fg = 'white', font = controller.title_font, 
-                                 bg = parent['bg'], height = 3, width = 20, command = self.NotImpl).grid(column = 0, row = 59)
-        button_write = tk.Button(self, text = 'Write', fg = 'white', font = controller.title_font, 
-                                 bg = parent['bg'], height = 3, width = 20, command = self.NotImpl).grid(column = 1, row = 59)
-        button_manual = tk.Button(self, text = 'Manual input', fg = 'white', font = controller.title_font, 
-                                  bg = parent['bg'], height = 3, width = 20, 
-                                  command = lambda:  controller.show_frame("PageManual")).grid(column = 2, row = 59)
-        button_canvas = tk.Button(self, text = 'Interact with canvas', fg = 'white', font = controller.title_font, 
-                                  bg = parent['bg'], height = 3, width = 20, 
-                                  command = lambda:  controller.show_frame("PageCanvas")).grid(column = 3, row = 59)
-        button_upload = tk.Button(self, text = 'Upoad file', fg = 'white', font = controller.title_font, 
-                                  bg = parent['bg'], height = 3, width = 20, 
-                                  command = self.upload_data).grid(column = 0, row = 60)
+        button_speak = tk.Button(
+            self, text = 'Speak', fg = 'white', font = controller.title_font, 
+            bg = parent['bg'], height = 3, width = 20, 
+            command = self.NotImpl).grid(column = 0, row = 59)
+        button_write = tk.Button(
+            self, text = 'Write', fg = 'white', font = controller.title_font, 
+            bg = parent['bg'], height = 3, width = 20, 
+            command = self.NotImpl).grid(column = 1, row = 59)
+        button_manual = tk.Button(
+            self, text = 'Manual input', fg = 'white', 
+            font = controller.title_font, bg = parent['bg'], height = 3, 
+            width = 20, command = lambda:  controller.show_frame(
+                "PageManual")).grid(column = 2, row = 59)
+        button_canvas = tk.Button(
+            self, text = 'Interact with canvas', fg = 'white', 
+            font = controller.title_font, bg = parent['bg'], height = 3, 
+            width = 20, command = lambda:  controller.show_frame(
+                "PageCanvas")).grid(column = 3, row = 59)
+        button_upload = tk.Button(
+            self, text = 'Upoad file', fg = 'white', 
+            font = controller.title_font, bg = parent['bg'], height = 3, 
+            width = 20, command = self.upload_data).grid(column = 0, row = 60)
         
     # def light_theme(self):
     #     listbox_tasks.config(bg="white", fg="black")
@@ -91,25 +105,17 @@ class StartPage(tk.Frame):
     #     entry_task.config(bg='white', fg='black')       
         
     def NotImpl(self):
-        messagebox.showwarning("Error", "This functionality is not implemented yet.")
+        messagebox.showwarning(
+            "Error", "This functionality is not implemented yet.")
         
     def upload_data(self):
-        filename = askopenfilename(initialdir = os.getcwd(), title = 'Select a file', defaultextension = '.csv', 
-                                   filetypes = [('Text file', '.txt'), ('CSS file', '.css'), ('All Files', '*.*')])
+        filename = askopenfilename(
+            initialdir = os.getcwd(), title = 'Select a file', 
+            defaultextension = '.csv', filetypes = [('Text file', '.txt'), 
+                                                    ('CSS file', '.css'), 
+                                                    ('All Files', '*.*')])
         if filename is not None:
             messagebox.showinfo("Success", "Feedback correctly uploaded.")
-
-# class PageCanvas(tk.Frame):
-
-#     def __init__(self, parent, controller):
-#         tk.Frame.__init__(self, parent, bg = parent['bg'])
-#         self.controller = controller
-#         label = tk.Label(self, text="This is page 2", font=controller.title_font)
-#         label.pack(side="top", fill="x", pady=10)
-#         button = tk.Button(self, text="Go to the start page",
-#                            command=lambda: controller.show_frame("StartPage"))
-#         button.pack()
-
 
 if __name__ == "__main__":
     app = UFA()
