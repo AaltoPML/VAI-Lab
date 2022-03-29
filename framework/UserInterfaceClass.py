3357154f4b9860f359e8e7deb5e31480413ebeac
Carlos Sevilla Salcedo <carlossevillasalcedo@gmail.com>
2022-03-29 13:27:18 +0300

    Branch updated to present model structure

---
+++ framework/UserInterfaceClass.py
@@ -0,0 +1,9 @@
+from abc import ABC
+
+"""Defines the parent class for all potential methods of user interface for the framework
+
+Todo: declare what is commonly needed for this module and add below (WIP)
+"""
+
+class UI(ABC):
+    pass
