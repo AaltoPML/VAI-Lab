3813ac5e2d209315a625eb0d8f5df554e19c98b1
cam586 <c.mcgreavy@hotmail.com>
2022-03-31 16:45:04 +0300

    new functions to main to be compatible with AC

--- aidesign/plugins/UI/__init__.py
+++ aidesign/plugins/UI/__init__.py
@@ -3,5 +3,5 @@
 from .. import __modules__
 from .canvas_input import PageCanvas
 from .manual_input import PageManual
-from .main import UFA
+from .main import Container
 from .main import StartPage
