033bcb163e20549172ccf11ae2b824024ec44478
Sevilla Salcedo Carlos <carlossevillasalcedo@gmail.com>
2022-04-07 13:03:31 +0300

    Flowchart save and reset
    Three new buttons added:
    - `reset`: eliminates all modules and connections in the canvas. It keeps the initialisation module, as it is always needed.
    - `save`: saves a matrix containing the conections between the modules. By default it saves it as an XML file. We are still working on the best file format.
    - `upload`: asks for an XML to upload and constructs a flowchart based on that file. (Not working yet)

--- aidesign/utils/plugins/aidCanvas.py
+++ aidesign/utils/plugins/aidCanvas.py
@@ -1,10 +1,11 @@
 # Import the required libraries
 import tkinter as tk
-from tkinter import font  as tkfont # python 3
 from PIL import Image, ImageTk
 import os
 import numpy as np
 import pandas as pd
+from tkinter.filedialog import asksaveasfile, askopenfile, askopenfilename
+from tkinter import messagebox
 
 class aidCanvas(tk.Frame):
 
@@ -40,34 +41,33 @@ class aidCanvas(tk.Frame):
                          padx = 10, pady = 10)
         
         self.canvas.startxy = []
-        
         self.out_data = pd.DataFrame(np.zeros((1,1)), 
                                      columns = ['Initialisation'], 
                                      index = ['Initialisation'])
         self.connections = {}
         self.connections[0] = {}
-        # Create plugin
+        # Create module
         self.w, self.h = 100, 50
         x0 = self.width/2 - (
             self.controller.pages_font.measure('Initialisation')+10)/2
-        y0 = 50
+
         self.cr = 4
         
-        self.canvas.create_rectangle(x0, y0, x0 + self.w, y0 + self.h, 
+        self.canvas.create_rectangle(x0, self.h, x0 + self.w, 2*self.h, 
                                      tags = 'p0', fill = self.bg, width = 3,
                                      activefill = '#dbaa21')
-        self.canvas.create_text(x0 + self.w/2, y0 + self.h/2, 
+        self.canvas.create_text(x0 + self.w/2, 3*self.h/2, 
                                 text = 'Initialisation', tags = 't0', 
                                 fill = '#d0d4d9', 
                                 font = self.controller.pages_font)
         self.canvas.create_oval(
-                        x0 + self.w/2-self.cr, y0 + self.h-self.cr, 
-                        x0 + self.w/2+self.cr, y0 + self.h+self.cr, 
+                        x0 + self.w/2-self.cr, 2*self.h-self.cr, 
+                        x0 + self.w/2+self.cr, 2*self.h+self.cr, 
                         width=2, fill = 'black', tags='d0')
-        self.canvas.tag_bind('d0', "<Button-1>", self.join_plugins)
+        self.canvas.tag_bind('d0', "<Button-1>", self.join_modules)
         self.draw = False
         self.canvas.startxy.append((x0 + self.w/2, 
-                                    y0 + self.h/2))
+                                    3*self.h/2))
         
         self.canvas.bind("<B1-Motion>", self.on_drag)
         self.canvas.bind('<Button-1>', self.select)
@@ -79,27 +79,45 @@ class aidCanvas(tk.Frame):
         tk.Button(
             self, text = 'Data preprocessing', fg = 'white', bg = parent['bg'],
             height = 3, width = 25, font = self.controller.pages_font,
-            command = lambda: self.add_plugin('Data preprocessing')).grid(column = 5, row = 1)
+            command = lambda: self.add_module('Data preprocessing')
+            ).grid(column = 5, row = 1)
         tk.Button(
             self, text = 'Modelling', fg = 'white', bg = parent['bg'],
             height = 3, width = 25, font = self.controller.pages_font,
-            command = lambda: self.add_plugin('Modelling')).grid(column = 5, row = 2)
+            command = lambda: self.add_module('Modelling')
+            ).grid(column = 5, row = 2)
         tk.Button(
             self, text = 'Decision making', fg = 'white', bg = parent['bg'],
             height = 3, width = 25, font = self.controller.pages_font,
-            command = lambda: self.add_plugin('Decision making')).grid(column = 5, row = 3)
+            command = lambda: self.add_module('Decision making')
+            ).grid(column = 5, row = 3)
         tk.Button(
             self, text = 'User Feedback Adaptation', fg = 'white', bg = parent['bg'],
             height = 3, width = 25, font = self.controller.pages_font,
-            command = lambda: self.add_plugin('User Feedback Adaptation')).grid(column = 5, row = 4)
-                
+            command = lambda: self.add_module('User Feedback Adaptation')
+            ).grid(column = 5, row = 4)
         tk.Button(
             self, text = 'Delete selection', fg = 'white', bg = parent['bg'],
             height = 3, width = 25, font = self.controller.pages_font,
             command = self.delete_sel).grid(column = 5, row = 5)
-
+        tk.Button(
+            self, text = 'Upload', fg = 'white', bg = parent['bg'], 
+            height = 3, width = 20, font = self.controller.pages_font, 
+            command = self.upload).grid(column = 0, row = 10, padx = 10)
+        tk.Button(
+            self, text = 'Save', fg = 'white', bg = parent['bg'],
+            height = 3, width = 20, font = self.controller.pages_font, 
+            command = self.save_file).grid(column = 1, row = 10)
+        tk.Button(
+            self, text = 'Reset', fg = 'white', bg = parent['bg'], 
+            height = 3, width = 20, font = self.controller.pages_font, 
+            command = self.reset).grid(column = 2, row = 10)
+        
+        self.save_path = ''
+        self.saved = True
+        
     def select(self, event):
-        """ Selects the plugin at the mouse location """
+        """ Selects the module at the mouse location. """
         self.selected = self.canvas.find_overlapping(
             event.x-5, event.y-5, event.x+5, event.y+5)
         if self.selected:
@@ -114,16 +132,18 @@ class aidCanvas(tk.Frame):
         self.m = int(self.canvas.gettags("current")[0][1:])
         
     def on_drag(self, event):
-        """ Uses the mouse location to move the plugin and its text."""
+        """ Uses the mouse location to move the module and its text. 
+        At the same time, it looks up if there are any connection to this
+        module and subsequently moves the connection."""
         
         self.select(event)
         
         self.m = int(self.canvas.gettags("current")[0][1:])
         dx = event.x - self.canvas.startxy[self.m][0]
         dy = event.y - self.canvas.startxy[self.m][1]
-        
-        self.canvas.move('o'+str(self.m), dx, dy) #Plugin
-        
+        # module
+        self.canvas.move('o'+str(self.m), dx, dy) 
+        # Connections
         if any(self.out_data.iloc[self.m].values) or any(
                 self.out_data[self.out_data.columns[self.m]].values):
             out = np.arange(self.out_data.values.shape[0])[
@@ -150,9 +170,15 @@ class aidCanvas(tk.Frame):
                          xycoord_i[1] + self.cr, 
                          xycoord_o[0] + self.cr, 
                          xycoord_o[1] + self.cr))
+        # Update module location
         self.canvas.startxy[self.m] = (event.x, event.y)
     
-    def plugin_out(self, name):
+    def module_out(self, name):
+        """ Updates the output DataFrame.
+        
+        :param name: name of the model
+        :type name: str
+        """
         name_list = list(self.out_data.columns)
         m_num = [n.split('-')[1] for n in name_list if n.split('-')[0]==name]
         name_list.append(name + '-' + str(len(m_num)))
@@ -164,8 +190,12 @@ class aidCanvas(tk.Frame):
                                      columns = name_list, 
                                      index = name_list)
     
-    def add_plugin(self, boxName):
-        """ Creates a plugin rectangle with the corresponding text inside."""
+    def add_module(self, boxName):
+        """ Creates a rectangular module with the corresponding text inside.
+        
+        :param boxName: name of the model
+        :type boxName: str
+        """
         text_w = self.controller.pages_font.measure(boxName)+10
         self.canvas.create_rectangle(
             self.width/2 - text_w/2 , 
@@ -192,7 +222,7 @@ class aidCanvas(tk.Frame):
             fill = 'black', 
             tags = ('o'+str(self.modules), 'd'+str(self.modules)))
         self.canvas.tag_bind('d'+str(self.modules), 
-                             "<Button-1>", self.join_plugins)
+                             "<Button-1>", self.join_modules)
         self.canvas.create_oval(
             self.width/2 - text_w/2 - self.cr, 
             self.height/2 - self.cr, 
@@ -202,7 +232,7 @@ class aidCanvas(tk.Frame):
             fill = 'black', 
             tags = ('o'+str(self.modules), 'l'+str(self.modules)))
         self.canvas.tag_bind('l'+str(self.modules), 
-                             "<Button-1>", self.join_plugins)
+                             "<Button-1>", self.join_modules)
         self.canvas.create_oval(
             self.width/2 - self.cr, 
             self.height/2 - self.h/2 - self.cr, 
@@ -212,7 +242,7 @@ class aidCanvas(tk.Frame):
             fill = 'black', 
             tags = ('o'+str(self.modules), 'u'+str(self.modules)))
         self.canvas.tag_bind('u'+str(self.modules), 
-                             "<Button-1>", self.join_plugins)
+                             "<Button-1>", self.join_modules)
         self.canvas.create_oval(
             self.width/2 + text_w/2 - self.cr, 
             self.height/2 - self.cr, 
@@ -222,23 +252,25 @@ class aidCanvas(tk.Frame):
             fill = 'black', 
             tags = ('o'+str(self.modules), 'r'+str(self.modules)))
         self.canvas.tag_bind('r'+str(self.modules), 
-                             "<Button-1>", self.join_plugins)
+                             "<Button-1>", self.join_modules)
         self.canvas.startxy.append((self.width/2, 
                                     self.height/2))
         self.connections[self.modules] = {}
         self.modules += 1
-        self.plugin_out(boxName)
-
+        self.module_out(boxName)
+        self.saved = False
+        
     def delete_sel(self):
-        """ Deletes last selected plugin"""
+        """ Deletes last selected module"""
         if self.canvas.selected and not(self.m == 0):
             self.canvas.delete('o'+str(self.m))
             
             col = self.out_data.columns[self.m]
             self.out_data[col] = 0
             self.out_data.loc[col] = 0
+            self.saved = False
                 
-    def join_plugins(self, event):
+    def join_modules(self, event):
         """ Draws a connecting line between two connecting circles. """
         if self.draw:
             tags = self.canvas.gettags(self.canvas.find_overlapping(
@@ -257,9 +289,11 @@ class aidCanvas(tk.Frame):
                             tags = ('o'+str(int(self.tag[1:])), 
                                   'o'+str(int(tag2[1:])), self.tag + '-' + tag2))
                 self.out_data.iloc[int(self.tag[1:])][int(tag2[1:])] = 1
+                print(self.out_data.values)
                 self.connections[
                     int(self.tag[1:])][int(tag2[1:])] = self.tag + '-' + tag2
             self.draw = False
+            self.saved = False
         else:
             tags = self.canvas.gettags(self.canvas.find_overlapping(
             event.x-self.cr, event.y-self.cr, 
@@ -268,8 +302,115 @@ class aidCanvas(tk.Frame):
             self.canvas.linestartxy = self.canvas.coords(self.tag)#(event.x, event.y)
             self.draw = True
 
-# AI Design
-# XML design
+    def save_file_as(self):
+        
+        self.save_path = asksaveasfile(mode='w')
+        self.save_file()
+        
+    def save_file(self):
+        
+        if self.save_path == '':
+            self.save_path = asksaveasfile(defaultextension = '.xml', filetypes = 
+                                      [('XML file', '.xml'), 
+                                       ('All Files', '*.*')])
+        if self.save_path is not None: # asksaveasfile return `None` if dialog closed with "cancel".
+            # Transform input for output file    
+            data = self.out_data.copy()
+            idx = (np.sum(data.values, axis = 0) 
+                   + np.sum(data.values, axis = 1)) < 1
+            col = data.columns
+            for c in np.arange(len(idx))[idx]:
+                data = data.drop(columns=col[c], index=col[c])
+            print(data.values)
+            
+            filedata = pd.DataFrame(data, columns = data.keys()).to_string()
+            self.save_path.seek(0) # Move to the first row to overwrite it
+            self.save_path.write(filedata)
+            self.save_path.flush() # Save without closing
+            # typically the above line would do. however this is used to ensure that the file is written
+            os.fsync(self.save_path.fileno())
+            self.saved = True
+         
+    def upload(self):
+        
+        ii = self.notebook.index(self.notebook.select())
+        filename = askopenfilename(initialdir = os.getcwd(), 
+                                   title = 'Select a file', 
+                                   defaultextension = '.txt', 
+                                   filetypes = [('Text file', '.txt'), 
+                                                ('CSS file', '.css'), 
+                                                ('All Files', '*.*')])
+        if filename is not None:
+            data = open(filename,'r')
+            read = False
+            self.draw[ii].set('drag')
+            for n, point in enumerate(data):
+                if read: # Not elegant at all, just to omit the header.
+                    i, sx, sy, ax, ay = point.split()
+                    # Draw an oval in the given coordinates
+                    self.canvas[ii].create_oval(
+                        float(sx)-3, float(sy)-3, float(sx)+3, float(sy)+3, 
+                        fill="black", width=0, 
+                        tags=("state-" + str(len(
+                            self.out_data[ii]['State_x']))))
+                    self.canvas[ii].create_line(
+                        float(sx), float(sy), float(ax), float(ay), 
+                        fill="red", arrow=tk.LAST, 
+                        tags=("action"+str(ii)+"-"+ str(len(
+                            self.out_data[ii]['Action_x']))))
+                    self.out_data[ii]['State_x'].append(sx)
+                    self.out_data[ii]['State_y'].append(sy)
+                    self.out_data[ii]['Action_x'].append(ax)
+                    self.out_data[ii]['Action_y'].append(ay)
+                    self.tree.insert(
+                        parent = '', index = 'end', 
+                        iid = len(self.out_data[ii]['Action_x'])-1, 
+                        text = len(self.out_data[ii]['Action_x']), 
+                        values = tuple(self.dict2mat(
+                            self.out_data[ii])
+                            [len(self.out_data[ii]
+                                 ['Action_x'])-1,:].astype(int)))
+                    self.tree.selection_set(str(len(
+                        self.out_data[ii]['Action_x'])-1))
+                else:
+                    read = True
+    
+    def reset(self):
+        
+        msg = messagebox.askyesnocancel(
+            'Info', 'Are you sure you want to reset the canvas?')
+        if msg:
+            self.canvas.delete(tk.ALL) # Reset canvas
+            
+            
+            self.out_data = pd.DataFrame(np.zeros((1,1)), 
+                                         columns = ['Initialisation'], 
+                                         index = ['Initialisation'])
+            x0 = self.width/2 - (
+            self.controller.pages_font.measure('Initialisation')+10)/2
+            y0 = self.h
+            self.canvas.create_rectangle(x0, y0, x0 + self.w, y0 + self.h, 
+                                     tags = 'p0', fill = self.bg, width = 3,
+                                     activefill = '#dbaa21')
+            self.canvas.create_text(x0 + self.w/2, y0 + self.h/2, 
+                                    text = 'Initialisation', tags = 't0', 
+                                    fill = '#d0d4d9', 
+                                    font = self.controller.pages_font)
+            self.canvas.create_oval(
+                            x0 + self.w/2-self.cr, y0 + self.h-self.cr, 
+                            x0 + self.w/2+self.cr, y0 + self.h+self.cr, 
+                            width=2, fill = 'black', tags='d0')
+            self.canvas.tag_bind('d0', "<Button-1>", self.join_modules)
+            self.draw = False
+            
+            self.canvas.startxy = []
+            self.canvas.startxy.append((x0 + self.w/2, 
+                                        y0 + self.h/2))
+            self.connections = {}
+            self.connections[0] = {}
+            self.modules = 1
+            self.saved = False
+    
 if __name__ == "__main__":
     app = aidCanvas()
     app.mainloop()
