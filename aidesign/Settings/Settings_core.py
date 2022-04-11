17d07a5c4af30fa1a954ff97c1343bd0af86cc26
cam586 <c.mcgreavy@hotmail.com>
2022-04-11 18:24:03 +0300

    added docstrings and cleaned up

--- aidesign/Settings/Settings_core.py
+++ aidesign/Settings/Settings_core.py
@@ -4,17 +4,26 @@ from ast import literal_eval
 
 
 class Settings(object):
-    def __init__(self):
+    def __init__(self, filename: str = None):
+        """Loads XML file and parses into nested dictionaries
+        This module is used as the backbone for all future modules.
+        If filename is not given, can be passed straight to self.load_XML()
+
+        :param filename [optional]: filename from which to load XML
+        """
         self.tree = None
         self.pipeline_tree = None
+        self.loaded_modules = {}
+
         self.data_tree = None
+        self.loaded_data_options = {}
 
         self.initialisation_options = {}
         self.output_options = {}
 
-        self.loaded_modules = {}
-        self.loaded_data_options = {}
-
+        """valid_tags lists the available XML tags and their function
+        TODO: populate the modules in this list automatically
+        """
         self.valid_tags = {
             "pipeline": "declaration",
             "datastructure": "declaration",
@@ -24,6 +33,9 @@ class Settings(object):
             "DataProcessing": "module",
             "loop": "loop"
         }
+
+        if filename is not None:
+            self.load_XML(filename)
 
     def load_XML(self, filename: str):
         """Loads XML file into class. Converts relative paths into absolute first.
@@ -44,10 +56,16 @@ class Settings(object):
         self.parse_data_structure()
 
     def parse_tags(self, element, parent):
+        """Detect tags and send them to correct method for parsing
+        Uses getattr to call the correct method
+
+        :param elem: xml.etree.ElementTree.Element to be parsed
+        :param parent: dict or dict fragment parsed tags will be appened to
+        """
         for child in element:
             try:
                 tag_type = self.valid_tags[child.tag]
-                getattr(self, "add_{}".format(tag_type))(child, parent)
+                getattr(self, "load_{}".format(tag_type))(child, parent)
             except KeyError:
                 print("\nError: Invalid XML Tag.")
                 print("XML tag \"{0}\" not found".format(child.tag))
@@ -55,7 +73,12 @@ class Settings(object):
                 print("\t- {}".format(",\n\t- ".join([*self.valid_tags])))
                 print("\n")
 
-    def add_module(self, element, parent):
+    def load_module(self, element, parent):
+        """Parses tags associated with modules and appends to parent dict
+
+        :param elem: xml.etree.ElementTree.Element to be parsed
+        :param parent: dict or dict fragment parsed tags will be appened to
+        """
         module_name = element.attrib["name"]
         module_type = element.tag
         plugin = element.find("plugin")
@@ -65,10 +88,15 @@ class Settings(object):
             "plugin_name": plugin_name,
             "options": {}
         }
-        self.parse_plugin_options(plugin, parent[module_name]["options"])
-        self.add_relationships(element, parent[module_name])
+        self.load_plugin_options(plugin, parent[module_name]["options"])
+        self.load_relationships(element, parent[module_name])
 
-    def parse_plugin_options(self, element, parent):
+    def load_plugin_options(self, element, parent):
+        """Parses tags associated with plugins and appends to parent dict
+
+        :param elem: xml.etree.ElementTree.Element to be parsed
+        :param parent: dict or dict fragment parsed tags will be appened to
+        """
         for child in element:
             if child.text != None:
                 val = self.parse_text_to_list(child)
@@ -76,23 +104,38 @@ class Settings(object):
                 val = child.attrib["value"]
             parent[child.tag] = val
 
-    def add_entry_point(self, element, parent):
+    def load_entry_point(self, element, parent):
+        """Parses tags associated with initialiser and appends to parent dict
+
+        :param elem: xml.etree.ElementTree.Element to be parsed
+        :param parent: dict or dict fragment parsed tags will be appened to
+        """
         initialiser_name = element.attrib["name"]
         parent[initialiser_name] = {}
         init_data_file = element.find("initial_data").attrib["file"]
         goal_data_file = element.find("goal").attrib["file"]
         parent[initialiser_name]["init_data_file"] = init_data_file
         parent[initialiser_name]["goal_data_file"] = goal_data_file
-        self.add_relationships(element, parent[initialiser_name])
+        self.load_relationships(element, parent[initialiser_name])
 
-    def add_exit_point(self, element, parent):
+    def load_exit_point(self, element, parent):
+        """Parses tags associated with output and appends to parent dict
+
+        :param elem: xml.etree.ElementTree.Element to be parsed
+        :param parent: dict or dict fragment parsed tags will be appened to
+        """
         parent["output"] = {}
         parent["output"]["save_fields"] = self.parse_text_to_list(
             element.find("out_data"))
         parent["output"]["save_dir"] = element.find("save_to").attrib["file"]
-        self.add_relationships(element, parent["output"])
+        self.load_relationships(element, parent["output"])
 
-    def add_loop(self, element, parent):
+    def load_loop(self, element, parent):
+        """Parses tags associated with loops and appends to parent dict
+
+        :param elem: xml.etree.ElementTree.Element to be parsed
+        :param parent: dict or dict fragment parsed tags will be appened to
+        """
         loop_name = element.attrib["name"]
         parent[loop_name] = {
             "type": element.attrib["type"],
@@ -100,7 +143,12 @@ class Settings(object):
         }
         self.parse_tags(element, parent[loop_name])
 
-    def add_relationships(self, element, parent):
+    def load_relationships(self, element, parent):
+        """Parses tags associated with relationships and adds to parent dict
+
+        :param elem: xml.etree.ElementTree.Element to be parsed
+        :param parent: dict or dict fragment parsed tags will be appened to
+        """
         parent["parents"] = []
         parent["children"] = []
         for rel in element.find("relationships"):
@@ -110,12 +158,18 @@ class Settings(object):
                 parent["children"].append(rel.attrib["name"])
 
     def parse_data_structure(self):
+        """Parses tags associated with data structure"""
         self.data_tree = self.root.find("datastructure")
         for child in self.data_tree:
             self.loaded_data_options[child.tag]\
                 = self.parse_text_to_list(child)
 
     def parse_text_to_list(self, element: ET.Element) -> list:
+        """Formats raw text data
+
+        :param elem: xml.etree.ElementTree.Element to be parsed
+        :returns out: list containing parsed text data
+        """
         new = element.text.strip().replace(" ", "")
         out = new.split("\n")
         raw_elem_text = str()
@@ -127,25 +181,27 @@ class Settings(object):
         return out
 
     def print_pretty(self, element: ET.Element):
+        """Print indented dictionary to screen"""
         from pprint import PrettyPrinter
         pp = PrettyPrinter(sort_dicts=False, width=100)
         pp.pprint(element)
 
     def print_loaded_modules(self):
+        """Print indented pipeline specifications to screen"""
         self.print_pretty(self.loaded_modules)
 
     def print_loaded_data_structure(self):
+        """Print indented data structure specifications to screen"""
         self.print_pretty(self.loaded_data_options)
 
     def write_to_XML(self):
         """Formats XML Tree correctly, then writes to filename
+        TODO: add overwrite check and alternate filename option
         """
         self.indent(self.root)
         self.tree.write(self.filename)
 
-    def indent(self,
-               elem: ET.Element,
-               level: int = 0):
+    def indent(self, elem: ET.Element, level: int = 0):
         """Formats XML tree to be human-readable before writing to file
 
         :param elem: xml.etree.ElementTree.Element to be indented
@@ -166,8 +222,8 @@ class Settings(object):
             if level and (not elem.tail or not elem.tail.strip()):
                 elem.tail = i
             if elem.text:
-                elem.text = elem.text.replace("\n",("\n" + (level+1)*sep))
-                elem.text = ("{0}{1}").format(elem.text,i)
+                elem.text = elem.text.replace("\n", ("\n" + (level+1)*sep))
+                elem.text = ("{0}{1}").format(elem.text, i)
 
     def append_pipeline_module_to_file(self,
                                        module_type: str,
@@ -180,7 +236,6 @@ class Settings(object):
         :param module_name: string give module a user-defined name
         :param plugin_type: string type of plugin to be loaded into module
         :param plugin_options: dict where keys & values are options & values
-        TODO: This is very specific to the GUI module - need to make more generic
         """
         new_mod = ET.Element(module_type)
         new_mod.set('name', module_name)
@@ -188,15 +243,14 @@ class Settings(object):
         new_plugin = ET.SubElement(new_mod, "plugin")
         new_plugin.set('type', plugin_type)
 
-        # new_options = 
         for key in plugin_options.keys():
             new_option = ET.SubElement(new_plugin, key)
-            if isinstance(plugin_options[key],list):
-                # option_text = ("\n\t\t\t{}\n\t\t".format("\n\t\t\t".join([*plugin_options[key]])))
-                option_text = ("\n{}".format("\n".join([*plugin_options[key]])))
+            if isinstance(plugin_options[key], list):
+                option_text = ("\n{}".format(
+                    "\n".join([*plugin_options[key]])))
                 new_option.text = option_text
 
-            elif isinstance(plugin_options[key],str):
+            elif isinstance(plugin_options[key], str):
                 new_option.set('value', plugin_options[key])
 
         self.pipeline_tree.append(new_mod)
@@ -206,6 +260,12 @@ class Settings(object):
                                             field_type: str,
                                             value: list,
                                             field_name: str = None):
+        """Add new tags for data structure to XML file
+
+        :param field_type: str of desired XML tag to add
+        :param value: list of values for field_type
+        :param field_name [optional]: str with user defined name for tag
+        """
         new_field = ET.Element(field_type)
         if field_name is not None:
             new_field.set('name', field_name)
@@ -214,10 +274,12 @@ class Settings(object):
         self.write_to_XML()
 
 
-s = Settings()
-s.load_XML("./resources/example_config.xml")
-s.write_to_XML()
-# s.print_loaded_modules()
+s = Settings("./resources/example_config.xml")
+s.print_loaded_modules()
+
+# Use case examples:
+# s.load_XML("./resources/example_config.xml")
+# s.write_to_XML()
 # s.append_pipeline_module_to_file("GUI",
 #                       "added_mod",
 #                       "startpage",
