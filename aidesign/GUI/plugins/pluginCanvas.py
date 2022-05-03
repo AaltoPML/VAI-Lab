c49b53a163ca56cdfdb4abf0f8bcda65831dac25
Carlos Sevilla Salcedo <carlossevillasalcedo@gmail.com>
2022-05-03 10:04:30 +0300

    Working info window

--- aidesign/GUI/plugins/pluginCanvas.py
+++ aidesign/GUI/plugins/pluginCanvas.py
@@ -224,6 +224,7 @@ class pluginCanvas(tk.Frame):
                 selectcolor = 'black', value = plug,
                     font = self.controller.pages_font)
             rb.grid(column = 5+ (p%2 != 0), row = int(p/2)+1)
+            CreateToolTip(rb, text = 'Hello there '+name+' '+plug)
             self.allWeHearIs.append(rb)
 
     def module_out(self, name):
@@ -559,7 +560,7 @@ class pluginCanvas(tk.Frame):
             self.controller._show_frame("MainPage")
 
 class ToolTip(object):
-    """ Creates a text window associated to a widget with a description. """
+    """ Defines a text window associated to a widget with a description. """
     
     def __init__(self, widget):
         self.widget = widget
@@ -573,8 +574,8 @@ class ToolTip(object):
         if self.tipwindow or not self.text:
             return
         x, y, cx, cy = self.widget.bbox("insert")
-        x = x + self.widget.winfo_rootx() + 57
-        y = y + cy + self.widget.winfo_rooty() +27
+        x = x + self.widget.winfo_rootx() + 40
+        y = y + cy + self.widget.winfo_rooty() #+ 17
         self.tipwindow = tw = tk.Toplevel(self.widget)
         tw.wm_overrideredirect(1)
         tw.wm_geometry("+%d+%d" % (x, y))
@@ -589,6 +590,16 @@ class ToolTip(object):
         if tw:
             tw.destroy()
 
+def CreateToolTip(widget, text):
+    """ Calls ToolTip to create a window with a widget description. """
+    toolTip = ToolTip(widget)
+    def enter(event):
+        toolTip.showtip(text)
+    def leave(event):
+        toolTip.hidetip()
+    widget.bind('<Enter>', enter)
+    widget.bind('<Leave>', leave)
+    
 if __name__ == "__main__":
     app = pluginCanvas()
     app.mainloop()
