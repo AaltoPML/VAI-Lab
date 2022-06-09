d7a60863782663bb1cf27b23fdf75fb0efa055ab
cam586 <c.mcgreavy@hotmail.com>
2022-06-09 11:20:44 +0300

    removed unneeded files

--- run_UI.py
+++ run_UI.py
@@ -5,9 +5,3 @@ core = ai.Core()
 core.load_config_file("./Data/resources/xml_files/data_passing_canvas_test.xml")
 # core.launch()
 core.run()
-
-# ['State_a', 'Action_a']
-# ['State_x', 'State_y', 'Action_x', 'Action_y']
-# [['State_a', 'Action_a'],['State_x', 'State_y', 'Action_x', 'Action_y']]
-
-# ['a', 'b', 'c', 'd']
