e008078155efe2424d30a2bb7b0cccff4cd9f6c4
Carlos Sevilla Salcedo <carlossevillasalcedo@gmail.com>
2022-08-25 13:46:15 +0300

    ToolTip included
    Once your mouse hovers over a module, it displays the information stored in the status dict.

--- aidesign/utils/plugins/progressTracker.py
+++ aidesign/utils/plugins/progressTracker.py
@@ -146,6 +146,12 @@ class progressTracker(tk.Frame):
                                      index = name_list)
         self.module_names.append(name_list[-1])
 
+    def pretty_status(self,status: dict):
+        status_str = ''
+        for key in status.keys():
+            status_str += str(key) +': '+str(status[key])+'\n'
+        return status_str
+
     def add_module(self,boxName: str,x: float,y:float,ini = False,out = False):
         """ Creates a rectangular module with the corresponding text inside.
         
@@ -180,6 +186,7 @@ class progressTracker(tk.Frame):
             width = 3,
             # activefill = '#dbaa21'
             )
+        
         self.canvas.create_text(
             x, 
             y, 
@@ -188,6 +195,11 @@ class progressTracker(tk.Frame):
             tags = tag + ('t'+str(self.modules),), 
             fill = '#d0d4d9', 
             justify = tk.CENTER)
+        
+        CanvasTooltip(self.canvas, self.canvas.find_withtag('p'+str(self.modules))[0], 
+                           text = self.pretty_status(self.controller.status[boxName]))
+        CanvasTooltip(self.canvas, self.canvas.find_withtag('t'+str(self.modules))[0], 
+                           text = self.pretty_status(self.controller.status[boxName]))
         
         if not out:
             self.canvas.create_oval(
@@ -233,7 +245,17 @@ class progressTracker(tk.Frame):
         self.module_out(boxName)
         self.module_list.append(boxName)
         self.modules += 1
-
+        
+    # def CreateToolTip(self, widget, text):
+    #     """ Calls ToolTip to create a window with a widget description. """
+    #     toolTip = ToolTip(widget)
+    #     def enter(event):
+    #         toolTip.showtip(text)
+    #     def leave(event):
+    #         toolTip.hidetip()
+    #     widget.bind('<Enter>', enter)
+    #     widget.bind('<Leave>', leave)
+        
     def upload(self):
         """ Opens the XML file that was previously uploaded and places the 
         modules, loops and connections in the canvas."""
@@ -374,6 +396,138 @@ class progressTracker(tk.Frame):
         
         self.controller.destroy()
         
+class CanvasTooltip:
+    '''
+    It creates a tooltip for a given canvas tag or id as the mouse is
+    above it.
+
+    This class has been derived from the original Tooltip class I updated
+    and posted back to StackOverflow at the following link:
+
+    https://stackoverflow.com/questions/3221956/
+           what-is-the-simplest-way-to-make-tooltips-in-tkinter/
+           41079350#41079350
+
+    Alberto Vassena on 2016.12.10.
+    '''
+
+    def __init__(self, canvas, tag_or_id,
+                 *,
+                 bg='#FFFFEA',
+                 pad=(5, 3, 5, 3),
+                 text='canvas info',
+                 waittime=100,
+                 wraplength=250):
+        self.waittime = waittime  # in miliseconds, originally 500
+        self.wraplength = wraplength  # in pixels, originally 180
+        self.canvas = canvas
+        self.text = text
+        self.canvas.tag_bind(tag_or_id, "<Enter>", self.onEnter)
+        self.canvas.tag_bind(tag_or_id, "<Leave>", self.onLeave)
+        self.canvas.tag_bind(tag_or_id, "<ButtonPress>", self.onLeave)
+        self.bg = bg
+        self.pad = pad
+        self.id = None
+        self.tw = None
+
+    def onEnter(self, event=None):
+        self.schedule()
+
+    def onLeave(self, event=None):
+        self.unschedule()
+        self.hide()
+
+    def schedule(self):
+        self.unschedule()
+        self.id = self.canvas.after(self.waittime, self.show)
+
+    def unschedule(self):
+        id_ = self.id
+        self.id = None
+        if id_:
+            self.canvas.after_cancel(id_)
+
+    def show(self, event=None):
+        def tip_pos_calculator(canvas, label,
+                               *,
+                               tip_delta=(10, 5), pad=(5, 3, 5, 3)):
+
+            c = canvas
+
+            s_width, s_height = c.winfo_screenwidth(), c.winfo_screenheight()
+
+            width, height = (pad[0] + label.winfo_reqwidth() + pad[2],
+                             pad[1] + label.winfo_reqheight() + pad[3])
+
+            mouse_x, mouse_y = c.winfo_pointerxy()
+
+            x1, y1 = mouse_x + tip_delta[0], mouse_y + tip_delta[1]
+            x2, y2 = x1 + width, y1 + height
+
+            x_delta = x2 - s_width
+            if x_delta < 0:
+                x_delta = 0
+            y_delta = y2 - s_height
+            if y_delta < 0:
+                y_delta = 0
+
+            offscreen = (x_delta, y_delta) != (0, 0)
+
+            if offscreen:
+
+                if x_delta:
+                    x1 = mouse_x - tip_delta[0] - width
+
+                if y_delta:
+                    y1 = mouse_y - tip_delta[1] - height
+
+            offscreen_again = y1 < 0  # out on the top
+
+            if offscreen_again:
+                # No further checks will be done.
+
+                # TIP:
+                # A further mod might automagically augment the
+                # wraplength when the tooltip is too high to be
+                # kept inside the screen.
+                y1 = 0
+
+            return x1, y1
+
+        bg = self.bg
+        pad = self.pad
+        canvas = self.canvas
+
+        # creates a toplevel window
+        self.tw = tk.Toplevel(canvas.master)
+
+        # Leaves only the label and removes the app window
+        self.tw.wm_overrideredirect(True)
+
+        win = tk.Frame(self.tw,
+                       background=bg,
+                       borderwidth=0)
+        label = tk.Label(win,
+                          text=self.text,
+                          justify=tk.LEFT,
+                          background=bg,
+                          relief=tk.SOLID,
+                          borderwidth=0,
+                          wraplength=self.wraplength)
+
+        label.grid(padx=(pad[0], pad[2]),
+                   pady=(pad[1], pad[3]),
+                   sticky=tk.NSEW)
+        win.grid()
+
+        x, y = tip_pos_calculator(canvas, label)
+
+        self.tw.wm_geometry("+%d+%d" % (x, y))
+
+    def hide(self):
+        if self.tw:
+            self.tw.destroy()
+        self.tw = None
 
 if __name__ == "__main__":
     app = progressTracker()
