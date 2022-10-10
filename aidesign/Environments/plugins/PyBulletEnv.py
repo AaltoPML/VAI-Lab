7d8353acccdaf6842967906f5367dd556cd88357
cam586 <c.mcgreavy@hotmail.com>
2022-10-10 14:59:36 +0300

    ongoing pybullet/arg population changes

--- aidesign/Environments/plugins/PyBulletEnv.py
+++ aidesign/Environments/plugins/PyBulletEnv.py
@@ -1,6 +1,6 @@
-from aidesign._plugin_templates import EnvironmentPluginT
+# from aidesign._plugin_templates import EnvironmentPluginT
 from typing import Dict
-import pybullet as p
+from pybullet import *
 
 _PLUGIN_READABLE_NAMES = {"PyBullet":"default"}                                                 # type:ignore
 _PLUGIN_MODULE_OPTIONS = {}                                                                     # type:ignore
@@ -9,27 +9,36 @@ _PLUGIN_OPTIONAL_SETTINGS = {"neg_label": "int", "pos_label": "int","timestep":"
 _PLUGIN_REQUIRED_DATA = {}                                                                      # type:ignore
 _PLUGIN_OPTIONAL_DATA = {"X","Y","X_tst", 'Y_tst'}                                              # type:ignore
 
-class PyBullet(EnvironmentPluginT):
+# class PyBullet(EnvironmentPluginT):
+class PyBulletEnv():
     """
     Load and control the PyBullet Environment
     """
     def __init__(self) -> None:
-        super().__init__(globals())
+        # super().__init__(globals())
         self.model_ids:Dict = {}
         
     def connect(self):
-        physicsClient = p.connect(p.GUI)# p.DIRECT for non-graphical version
+        self.physicsClient = connect(GUI)# p.DIRECT for non-graphical version
 
     def disconnect(self):
-        return p.disconnect()
+        return disconnect()
 
     def reset(self):
-        return p.resetSimulation()
+        return resetSimulation()
 
     def load_model(self,model_path) -> None:
         self.model_ids[model_path] = p.loadURDF(model_path)
 
     def step(self, action):
-        return p.step(action)
+        return step(action)
+
+    def get_lib_args(self):
+        for i in dir(p):
+            print (i)
 
         
+if __name__ == "__main__":
+    pb = PyBulletEnv()
+    pb.connect()
+    # pb.get_lib_args()
