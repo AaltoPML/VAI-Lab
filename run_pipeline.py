d2c7150d3e2725e941850b2ba87bfc7dd5ca9eb6
cam586 <c.mcgreavy@hotmail.com>
2022-10-12 12:43:15 +0300

    modified run pipeline for pybullet

--- run_pipeline.py
+++ run_pipeline.py
@@ -2,9 +2,9 @@ import aidesign as ai
 
 core = ai.Core()
 
-# core.load_config_file(
-#     ("./examples",
-#     "xml_files",
-#     'SVR_demo.xml'))
+core.load_config_file(
+    ("./examples",
+    "xml_files",
+    'pybullet_env_example.xml'))
     
 core.run()
