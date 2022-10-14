4309763f0dc8b0ba0a95b73d8aa1c63e9dbcea79
cam586 <c.mcgreavy@hotmail.com>
2022-10-14 17:30:35 +0300

    added __getattr__ function to access all pybullet methods from ENV class

--- aidesign/Environment/plugins/PyBulletEnv.py
+++ aidesign/Environment/plugins/PyBulletEnv.py
@@ -1,7 +1,7 @@
 from aidesign._plugin_templates import EnvironmentPluginT
 from aidesign._import_helper import rel_to_abs
-from typing import Dict
-from pybullet import *
+from typing import Any, Dict, TYPE_CHECKING
+import pybullet as p
 from time import sleep
 
 
@@ -9,50 +9,52 @@ _PLUGIN_READABLE_NAMES = {"PyBullet":"default"}                                 
 _PLUGIN_MODULE_OPTIONS = {}                                                                     # type:ignore
 _PLUGIN_REQUIRED_SETTINGS = {"model_dir": "str", "headless": "bool",
                                 "timestep":"float", "max_steps":"int"}                          # type:ignore
-_PLUGIN_OPTIONAL_SETTINGS = {}                                                                  # type:ignore
+_PLUGIN_OPTIONAL_SETTINGS = {"gravity":"list"}                                                  # type:ignore
 _PLUGIN_REQUIRED_DATA = {}                                                                      # type:ignore
 _PLUGIN_OPTIONAL_DATA = {"X","Y","X_tst", 'Y_tst'}                                              # type:ignore
+
 
 class PyBulletEnv(EnvironmentPluginT):
     """
     Loads the pybullet library as wildcard and exposes all functions
     """
+
     def __init__(self) -> None:
         super().__init__(globals())
-        self.connection_mode = GUI
-        self.model_ids:Dict = {}
-        
-    def set_gui(self,use_gui:bool=True):
+        self.connection_mode = p.GUI
+        self.model_ids: Dict = {}
+
+    def set_gui(self, use_gui: bool = True):
         if use_gui:
-            self.connection_mode = GUI # Use pybullet GUI
+            self.connection_mode = p.GUI  # Use pybullet GUI
         else:
-            self.connection_mode = DIRECT # Use pybullet without GUI
+            self.connection_mode = p.DIRECT  # Use pybullet without GUI
 
     def connect(self):
-        self.physicsClient = connect(self.connection_mode)
+        self.physicsClient = p.connect(self.connection_mode)
 
     def disconnect(self):
-        return disconnect()
+        return p.disconnect()
 
     def reset(self):
-        return resetSimulation()
+        return p.resetSimulation()
 
     def load_pb_data(self):
         import pybullet_data                                        # type:ignore
-        setAdditionalSearchPath(pybullet_data.getDataPath())
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-    def _load_model_by_type(self,model):
+    def _load_model_by_type(self, model):
         model = rel_to_abs(model)
         ext = model.split(".")[-1]
         name = model.split("/")[-1].split(".")[0]
         if name == "plane":
-                self.load_pb_data()
+            self.load_pb_data()
         if ext == "urdf":
-            self.model_ids[name] = loadURDF(model)
+            self.model_ids[name] = p.loadURDF(model)
         elif ext == "sdf":
-            self.model_ids[name] = loadSDF(model,1,1.0)
+            self.model_ids[name] = p.loadSDF(model, 1, 1.0)
         elif ext == "xml":
-            self.model_ids[name] = loadMJCF(model)
+            self.model_ids[name] = p.loadMJCF(model)
 
     def load_model(self) -> None:
         model_paths = self._config["options"]["model_dir"]
@@ -61,25 +63,36 @@ class PyBulletEnv(EnvironmentPluginT):
         elif type(model_paths) == list:
             for model in model_paths:
                 self._load_model_by_type(model)
-            
 
     def _set_options(self):
         if "gravity" in self._config["options"]:
             g = self._config["options"]["gravity"]
-            setGravity(float(g[0]),float(g[1]),float(g[2]))
+            self.setGravity(float(g[0]), float(g[1]), float(g[2]))
         elif "timestep" in self._config["options"]:
-            setPhysicsEngineParameter(fixedTimeStep=self._config["options"]["timestep"])
+            p.setPhysicsEngineParameter(
+                fixedTimeStep=self._config["options"]["timestep"])
         elif "max_steps" in self._config["options"]:
-            setPhysicsEngineParameter(numSolverIterations=self._config["options"]["max_steps"])
+            p.setPhysicsEngineParameter(
+                numSolverIterations=self._config["options"]["max_steps"])
 
     def run_simulation(self):
         self._set_options()
-        for step in range(1,self._config["options"]["max_steps"]):
-            stepSimulation()
+        for step in range(1, self._config["options"]["max_steps"]):
+            p.stepSimulation()
             sleep(self._config["options"]["timestep"])
         self.disconnect()
 
+    def __getattr__(self, attr: str) -> Any:
+        """Allows calling pybullet functions directly as if they were functions of this class
+
+        TODO: This is probably not the best way to do this, but pybullet is
+        a compiled module and cannot be used as a parent. This is a workaround.
+        """
+        return getattr(p, attr)
+
+
 if __name__ == "__main__":
     pb = PyBulletEnv()
-    pb.set_gui(True)
-    pb.connect()
+    print(pb.ACTIVATION_STATE_DISABLE_SLEEPING)
+    # pb.set_gui(True)
+    # pb.connect()
