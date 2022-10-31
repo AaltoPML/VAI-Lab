cf28a8bb6b0871569e8339f7755973f183df8f1a
Carlos Sevilla Salcedo <carlossevillasalcedo@gmail.com>
2022-10-31 18:00:11 +0100

    argopt plugin

--- run_pipeline.py
+++ run_pipeline.py
@@ -2,9 +2,9 @@ import aidesign as ai
 
 core = ai.Core()
 
-# core.load_config_file(
-#     ("./examples",
-#     "xml_files",
-#     'crystalDesign_v1.xml'))
+core.load_config_file(
+    ("./examples",
+    "xml_files",
+    'crystalDesign_v2.xml'))
 
 core.run()
