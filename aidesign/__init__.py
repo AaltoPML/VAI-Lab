ecf40075ce714e22d15db81e316ee5a5cdb20552
cam586 <c.mcgreavy@hotmail.com>
2022-11-03 15:31:49 +0200

    cleaned up main __init__ file

--- aidesign/__init__.py
+++ aidesign/__init__.py
@@ -1,7 +1,1 @@
 from .Core import Core
-# from .Settings import Settings
-# from .utils import AID
-# from. import utils
-# from .UserFeedback import UserFeedback
-# from .Modelling import Modelling
-# from .GUI import GUI
