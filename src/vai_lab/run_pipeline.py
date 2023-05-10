7ae51a9c27708171bad495061dcd8866f11ddefa
Carlos Sevilla Salcedo <carlossevillasalcedo@gmail.com>
2023-05-10 14:15:23 +0200

    Fix issue with example

--- src/vai_lab/run_pipeline.py
+++ src/vai_lab/run_pipeline.py
@@ -57,9 +57,10 @@ def main():
         for i in range(0,len(args.file)):
             args.file[i] = abspath(args.file[i])
         core.load_config_file(args.file)
-    # core.load_config_file(("./examples",
-    #                         "xml_files",
-    #                         'bayes_opt_demo.xml'))
+    core.load_config_file(("./examples",
+                            "xml_files",
+                            # 'bayes_opt_demo.xml'))
+                            'optimisation_demo.xml'))
     # Run pipeline   
     core.run()
 
